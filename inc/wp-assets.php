1a19859e5733ec1b7229de94d66bbfa1cf34f1a3
Ian Del Giudice <ian.pvd@gmail.com>
2017-06-19 01:34:57 -0400

    fix paths to wp vendor js

--- inc/wp-assets.php
+++ inc/wp-assets.php
@@ -13,7 +13,7 @@
  * @return void
  */
 function roundhouse_wp_assets() {
-	wp_enqueue_script( 'roundhouse-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-	wp_enqueue_script( 'roundhouse-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'roundhouse-navigation', get_template_directory_uri() . '/client/js/vendor/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'roundhouse-skip-link-focus-fix', get_template_directory_uri() . '/client/js/vendor/skip-link-focus-fix.js', array(), '20151215', true );
 }
 add_action( 'wp_enqueue_scripts', 'roundhouse_wp_assets' );
