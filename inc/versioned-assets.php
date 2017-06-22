f8e1778876a7d2ef437979a492f830fda1de7639
Ian Del Giudice <ian.pvd@gmail.com>
2017-06-22 10:24:01 -0400

    move theme options to

--- inc/versioned-assets.php
+++ inc/versioned-assets.php
@@ -8,15 +8,15 @@
 */
 function roundhouse_get_versioned_asset( $asset ) {
 	$assets = array(
-		'site-js' => 'js/site.bundle.js',
-		'site-css' => 'css/site.css',
-		'article-js' => 'js/article.bundle.js',
-		'article-css' => 'css/article.css',
-		'archive-js' => 'js/archive.bundle.js',
-		'archive-css' => 'css/archive.css',
-		'admin-js' => 'js/admin.bundle.js',
-		'admin-css' => 'css/admin.css',
-		'common-js' => 'js/common.bundle.js',
+		'site-js' => 'js/site.89ac9e1.bundle.min.js',
+		'site-css' => 'css/site.db822f1c73953948c02b.min.css',
+		'article-js' => 'js/article.89ac9e1.bundle.min.js',
+		'article-css' => 'css/article.db822f1c73953948c02b.min.css',
+		'archive-js' => 'js/archive.89ac9e1.bundle.min.js',
+		'archive-css' => 'css/archive.db822f1c73953948c02b.min.css',
+		'admin-js' => 'js/admin.89ac9e1.bundle.min.js',
+		'admin-css' => 'css/admin.db822f1c73953948c02b.min.css',
+		'common-js' => 'js/common.89ac9e1.bundle.min.js',
 	);
 	return ! empty( $assets[ $asset ] ) ? $assets[ $asset ] : false;
 }
