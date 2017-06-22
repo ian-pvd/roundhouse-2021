ebb2426f101fefcdda9d2c7cad8dd77191ab10cd
Ian Del Giudice <ian.pvd@gmail.com>
2017-06-22 14:23:23 -0400

    enables webpack dev

--- inc/versioned-assets.php
+++ inc/versioned-assets.php
@@ -8,15 +8,15 @@
 */
 function roundhouse_get_versioned_asset( $asset ) {
 	$assets = array(
-		'site-js' => 'js/site.89ac9e1.bundle.min.js',
-		'site-css' => 'css/site.db822f1c73953948c02b.min.css',
-		'article-js' => 'js/article.89ac9e1.bundle.min.js',
-		'article-css' => 'css/article.db822f1c73953948c02b.min.css',
-		'archive-js' => 'js/archive.89ac9e1.bundle.min.js',
-		'archive-css' => 'css/archive.db822f1c73953948c02b.min.css',
-		'admin-js' => 'js/admin.89ac9e1.bundle.min.js',
-		'admin-css' => 'css/admin.db822f1c73953948c02b.min.css',
-		'common-js' => 'js/common.89ac9e1.bundle.min.js',
+		'site-js' => 'js/site.ae0b963.bundle.min.js',
+		'site-css' => 'css/site.8fb5d0fe18278a78d7cb.min.css',
+		'article-js' => 'js/article.ae0b963.bundle.min.js',
+		'article-css' => 'css/article.8fb5d0fe18278a78d7cb.min.css',
+		'archive-js' => 'js/archive.ae0b963.bundle.min.js',
+		'archive-css' => 'css/archive.8fb5d0fe18278a78d7cb.min.css',
+		'admin-js' => 'js/admin.ae0b963.bundle.min.js',
+		'admin-css' => 'css/admin.8fb5d0fe18278a78d7cb.min.css',
+		'common-js' => 'js/common.ae0b963.bundle.min.js',
 	);
 	return ! empty( $assets[ $asset ] ) ? $assets[ $asset ] : false;
 }
