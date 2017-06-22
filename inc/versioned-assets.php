fc7f735e06a308f2ebe0a3a85ed51e5f9d74e1d9
Ian Del Giudice <ian.pvd@gmail.com>
2017-06-22 19:21:26 -0400

    Adds front end bundles.

--- inc/versioned-assets.php
+++ inc/versioned-assets.php
@@ -8,15 +8,19 @@
 */
 function roundhouse_get_versioned_asset( $asset ) {
 	$assets = array(
-		'site-js' => 'js/site.ae0b963.bundle.min.js',
-		'site-css' => 'css/site.8fb5d0fe18278a78d7cb.min.css',
-		'article-js' => 'js/article.ae0b963.bundle.min.js',
-		'article-css' => 'css/article.8fb5d0fe18278a78d7cb.min.css',
-		'archive-js' => 'js/archive.ae0b963.bundle.min.js',
-		'archive-css' => 'css/archive.8fb5d0fe18278a78d7cb.min.css',
-		'admin-js' => 'js/admin.ae0b963.bundle.min.js',
-		'admin-css' => 'css/admin.8fb5d0fe18278a78d7cb.min.css',
-		'common-js' => 'js/common.ae0b963.bundle.min.js',
+		'site-js' => 'js/site.7428ab0.bundle.min.js',
+		'site-css' => 'css/site.e68a53d570e2a5b9677a.min.css',
+		'page-js' => 'js/page.7428ab0.bundle.min.js',
+		'page-css' => 'css/page.e68a53d570e2a5b9677a.min.css',
+		'home-js' => 'js/home.7428ab0.bundle.min.js',
+		'home-css' => 'css/home.e68a53d570e2a5b9677a.min.css',
+		'article-js' => 'js/article.7428ab0.bundle.min.js',
+		'article-css' => 'css/article.e68a53d570e2a5b9677a.min.css',
+		'archive-js' => 'js/archive.7428ab0.bundle.min.js',
+		'archive-css' => 'css/archive.e68a53d570e2a5b9677a.min.css',
+		'admin-js' => 'js/admin.7428ab0.bundle.min.js',
+		'admin-css' => 'css/admin.e68a53d570e2a5b9677a.min.css',
+		'common-js' => 'js/common.7428ab0.bundle.min.js',
 	);
 	return ! empty( $assets[ $asset ] ) ? $assets[ $asset ] : false;
 }
