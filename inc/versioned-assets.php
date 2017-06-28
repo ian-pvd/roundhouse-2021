4dbd8f3d584acf031003de42a64643f43548bbe4
Ian Del Giudice <ian.pvd@gmail.com>
2017-06-28 02:16:25 -0400

    fix: works without vendor navigation.js

--- inc/versioned-assets.php
+++ inc/versioned-assets.php
@@ -8,19 +8,19 @@
 */
 function roundhouse_get_versioned_asset( $asset ) {
 	$assets = array(
-		'site-js' => 'js/site.bundle.js',
-		'site-css' => 'css/site.css',
-		'page-js' => 'js/page.bundle.js',
-		'page-css' => 'css/page.css',
-		'home-js' => 'js/home.bundle.js',
-		'home-css' => 'css/home.css',
-		'article-js' => 'js/article.bundle.js',
-		'article-css' => 'css/article.css',
-		'archive-js' => 'js/archive.bundle.js',
-		'archive-css' => 'css/archive.css',
-		'admin-js' => 'js/admin.bundle.js',
-		'admin-css' => 'css/admin.css',
-		'common-js' => 'js/common.bundle.js',
+		'site-js' => 'js/site.627f0c6.bundle.min.js',
+		'site-css' => 'css/site.dcb7ebb496e40763edbc.min.css',
+		'page-js' => 'js/page.627f0c6.bundle.min.js',
+		'page-css' => 'css/page.dcb7ebb496e40763edbc.min.css',
+		'home-js' => 'js/home.627f0c6.bundle.min.js',
+		'home-css' => 'css/home.dcb7ebb496e40763edbc.min.css',
+		'article-js' => 'js/article.627f0c6.bundle.min.js',
+		'article-css' => 'css/article.dcb7ebb496e40763edbc.min.css',
+		'archive-js' => 'js/archive.627f0c6.bundle.min.js',
+		'archive-css' => 'css/archive.dcb7ebb496e40763edbc.min.css',
+		'admin-js' => 'js/admin.627f0c6.bundle.min.js',
+		'admin-css' => 'css/admin.dcb7ebb496e40763edbc.min.css',
+		'common-js' => 'js/common.627f0c6.bundle.min.js',
 	);
 	return ! empty( $assets[ $asset ] ) ? $assets[ $asset ] : false;
 }
