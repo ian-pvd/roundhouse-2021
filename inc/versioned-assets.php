48cbd58bf086474e3fb7f3ca95bf901b00006adf
Ian Del Giudice <ian.pvd@gmail.com>
2017-06-28 12:28:23 -0400

    don't remove link underline

--- inc/versioned-assets.php
+++ inc/versioned-assets.php
@@ -8,19 +8,19 @@
 */
 function roundhouse_get_versioned_asset( $asset ) {
 	$assets = array(
-		'site-js' => 'js/site.627f0c6.bundle.min.js',
-		'site-css' => 'css/site.dcb7ebb496e40763edbc.min.css',
-		'page-js' => 'js/page.627f0c6.bundle.min.js',
-		'page-css' => 'css/page.dcb7ebb496e40763edbc.min.css',
-		'home-js' => 'js/home.627f0c6.bundle.min.js',
-		'home-css' => 'css/home.dcb7ebb496e40763edbc.min.css',
-		'article-js' => 'js/article.627f0c6.bundle.min.js',
-		'article-css' => 'css/article.dcb7ebb496e40763edbc.min.css',
-		'archive-js' => 'js/archive.627f0c6.bundle.min.js',
-		'archive-css' => 'css/archive.dcb7ebb496e40763edbc.min.css',
-		'admin-js' => 'js/admin.627f0c6.bundle.min.js',
-		'admin-css' => 'css/admin.dcb7ebb496e40763edbc.min.css',
-		'common-js' => 'js/common.627f0c6.bundle.min.js',
+		'site-js' => 'js/site.4dbd8f3.bundle.min.js',
+		'site-css' => 'css/site.0f96cdbf8994b8d66a81.min.css',
+		'page-js' => 'js/page.4dbd8f3.bundle.min.js',
+		'page-css' => 'css/page.0f96cdbf8994b8d66a81.min.css',
+		'home-js' => 'js/home.4dbd8f3.bundle.min.js',
+		'home-css' => 'css/home.0f96cdbf8994b8d66a81.min.css',
+		'article-js' => 'js/article.4dbd8f3.bundle.min.js',
+		'article-css' => 'css/article.0f96cdbf8994b8d66a81.min.css',
+		'archive-js' => 'js/archive.4dbd8f3.bundle.min.js',
+		'archive-css' => 'css/archive.0f96cdbf8994b8d66a81.min.css',
+		'admin-js' => 'js/admin.4dbd8f3.bundle.min.js',
+		'admin-css' => 'css/admin.0f96cdbf8994b8d66a81.min.css',
+		'common-js' => 'js/common.4dbd8f3.bundle.min.js',
 	);
 	return ! empty( $assets[ $asset ] ) ? $assets[ $asset ] : false;
 }
