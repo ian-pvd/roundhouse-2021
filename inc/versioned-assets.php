deebcd5de70098cdf3fd42c335aae99baa086e8f
Ian Del Giudice <ian.pvd@gmail.com>
2017-07-26 16:17:06 -0400

    clean up home page column layout styles

--- inc/versioned-assets.php
+++ inc/versioned-assets.php
@@ -8,19 +8,19 @@
 */
 function roundhouse_get_versioned_asset( $asset ) {
 	$assets = array(
-		'site-js' => 'js/site.942e588.bundle.min.js',
-		'site-css' => 'css/site.fe2d2e8539985d498a49.min.css',
-		'page-js' => 'js/page.942e588.bundle.min.js',
-		'page-css' => 'css/page.fe2d2e8539985d498a49.min.css',
-		'home-js' => 'js/home.942e588.bundle.min.js',
-		'home-css' => 'css/home.fe2d2e8539985d498a49.min.css',
-		'article-js' => 'js/article.942e588.bundle.min.js',
-		'article-css' => 'css/article.fe2d2e8539985d498a49.min.css',
-		'archive-js' => 'js/archive.942e588.bundle.min.js',
-		'archive-css' => 'css/archive.fe2d2e8539985d498a49.min.css',
-		'admin-js' => 'js/admin.942e588.bundle.min.js',
-		'admin-css' => 'css/admin.fe2d2e8539985d498a49.min.css',
-		'common-js' => 'js/common.942e588.bundle.min.js',
+		'site-js' => 'js/site.bundle.js',
+		'site-css' => 'css/site.css',
+		'page-js' => 'js/page.bundle.js',
+		'page-css' => 'css/page.css',
+		'home-js' => 'js/home.bundle.js',
+		'home-css' => 'css/home.css',
+		'article-js' => 'js/article.bundle.js',
+		'article-css' => 'css/article.css',
+		'archive-js' => 'js/archive.bundle.js',
+		'archive-css' => 'css/archive.css',
+		'admin-js' => 'js/admin.bundle.js',
+		'admin-css' => 'css/admin.css',
+		'common-js' => 'js/common.bundle.js',
 	);
 	return ! empty( $assets[ $asset ] ) ? $assets[ $asset ] : false;
 }
