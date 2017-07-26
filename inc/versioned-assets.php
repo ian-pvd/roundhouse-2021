899db8b0821e02f88c383d3a7f5742d202e42e54
Ian Del Giudice <ian.pvd@gmail.com>
2017-07-26 16:19:07 -0400

    add build files.

--- inc/versioned-assets.php
+++ inc/versioned-assets.php
@@ -8,19 +8,19 @@
 */
 function roundhouse_get_versioned_asset( $asset ) {
 	$assets = array(
-		'site-js' => 'js/site.bundle.js',
-		'site-css' => 'css/site.css',
-		'page-js' => 'js/page.bundle.js',
-		'page-css' => 'css/page.css',
-		'home-js' => 'js/home.bundle.js',
-		'home-css' => 'css/home.css',
-		'article-js' => 'js/article.bundle.js',
-		'article-css' => 'css/article.css',
-		'archive-js' => 'js/archive.bundle.js',
-		'archive-css' => 'css/archive.css',
-		'admin-js' => 'js/admin.bundle.js',
-		'admin-css' => 'css/admin.css',
-		'common-js' => 'js/common.bundle.js',
+		'site-js' => 'js/site.deebcd5.bundle.min.js',
+		'site-css' => 'css/site.6e18b12d58b74061960f.min.css',
+		'page-js' => 'js/page.deebcd5.bundle.min.js',
+		'page-css' => 'css/page.6e18b12d58b74061960f.min.css',
+		'home-js' => 'js/home.deebcd5.bundle.min.js',
+		'home-css' => 'css/home.6e18b12d58b74061960f.min.css',
+		'article-js' => 'js/article.deebcd5.bundle.min.js',
+		'article-css' => 'css/article.6e18b12d58b74061960f.min.css',
+		'archive-js' => 'js/archive.deebcd5.bundle.min.js',
+		'archive-css' => 'css/archive.6e18b12d58b74061960f.min.css',
+		'admin-js' => 'js/admin.deebcd5.bundle.min.js',
+		'admin-css' => 'css/admin.6e18b12d58b74061960f.min.css',
+		'common-js' => 'js/common.deebcd5.bundle.min.js',
 	);
 	return ! empty( $assets[ $asset ] ) ? $assets[ $asset ] : false;
 }
