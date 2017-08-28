cb59bfbbf21e91a01721fb2e5934f8f4ebba8594
Ian Del Giudice <ian.pvd@gmail.com>
2017-08-28 14:24:38 -0400

    reconcile existing theme overrides

--- inc/versioned-assets.php
+++ inc/versioned-assets.php
@@ -8,19 +8,19 @@
 */
 function brigada71_get_versioned_asset( $asset ) {
 	$assets = array(
-		'site-js' => 'js/site.8313298.bundle.min.js',
-		'site-css' => 'css/site.a7c98e13facd94fa3eaa.min.css',
-		'page-js' => 'js/page.8313298.bundle.min.js',
-		'page-css' => 'css/page.a7c98e13facd94fa3eaa.min.css',
-		'home-js' => 'js/home.8313298.bundle.min.js',
-		'home-css' => 'css/home.a7c98e13facd94fa3eaa.min.css',
-		'article-js' => 'js/article.8313298.bundle.min.js',
-		'article-css' => 'css/article.a7c98e13facd94fa3eaa.min.css',
-		'archive-js' => 'js/archive.8313298.bundle.min.js',
-		'archive-css' => 'css/archive.a7c98e13facd94fa3eaa.min.css',
-		'admin-js' => 'js/admin.8313298.bundle.min.js',
-		'admin-css' => 'css/admin.a7c98e13facd94fa3eaa.min.css',
-		'common-js' => 'js/common.8313298.bundle.min.js',
+		'site-js' => 'js/site.d94436f.bundle.min.js',
+		'site-css' => 'css/site.fa620806588a794365b6.min.css',
+		'page-js' => 'js/page.d94436f.bundle.min.js',
+		'page-css' => 'css/page.fa620806588a794365b6.min.css',
+		'home-js' => 'js/home.d94436f.bundle.min.js',
+		'home-css' => 'css/home.fa620806588a794365b6.min.css',
+		'article-js' => 'js/article.d94436f.bundle.min.js',
+		'article-css' => 'css/article.fa620806588a794365b6.min.css',
+		'archive-js' => 'js/archive.d94436f.bundle.min.js',
+		'archive-css' => 'css/archive.fa620806588a794365b6.min.css',
+		'admin-js' => 'js/admin.d94436f.bundle.min.js',
+		'admin-css' => 'css/admin.fa620806588a794365b6.min.css',
+		'common-js' => 'js/common.d94436f.bundle.min.js',
 	);
 	return ! empty( $assets[ $asset ] ) ? $assets[ $asset ] : false;
 }
