1b6a0801e7ea83ca878ea425ec62da8a591217e6
Ian Del Giudice <ian.pvd@gmail.com>
2017-10-17 01:17:54 -0400

    split header customizations into src

--- inc/versioned-assets.php
+++ inc/versioned-assets.php
@@ -8,19 +8,19 @@
 */
 function roundhouse_get_versioned_asset( $asset ) {
 	$assets = array(
-		'site-js' => 'js/site.158f32c.bundle.min.js',
-		'site-css' => 'css/site.7e5ae794332b93eb61e9.min.css',
-		'page-js' => 'js/page.158f32c.bundle.min.js',
-		'page-css' => 'css/page.7e5ae794332b93eb61e9.min.css',
-		'home-js' => 'js/home.158f32c.bundle.min.js',
-		'home-css' => 'css/home.7e5ae794332b93eb61e9.min.css',
-		'article-js' => 'js/article.158f32c.bundle.min.js',
-		'article-css' => 'css/article.7e5ae794332b93eb61e9.min.css',
-		'archive-js' => 'js/archive.158f32c.bundle.min.js',
-		'archive-css' => 'css/archive.7e5ae794332b93eb61e9.min.css',
-		'admin-js' => 'js/admin.158f32c.bundle.min.js',
-		'admin-css' => 'css/admin.7e5ae794332b93eb61e9.min.css',
-		'common-js' => 'js/common.158f32c.bundle.min.js',
+		'site-js' => 'js/site.fab33fa.bundle.min.js',
+		'site-css' => 'css/site.55cd5352bd3c45728347.min.css',
+		'page-js' => 'js/page.fab33fa.bundle.min.js',
+		'page-css' => 'css/page.55cd5352bd3c45728347.min.css',
+		'home-js' => 'js/home.fab33fa.bundle.min.js',
+		'home-css' => 'css/home.55cd5352bd3c45728347.min.css',
+		'article-js' => 'js/article.fab33fa.bundle.min.js',
+		'article-css' => 'css/article.55cd5352bd3c45728347.min.css',
+		'archive-js' => 'js/archive.fab33fa.bundle.min.js',
+		'archive-css' => 'css/archive.55cd5352bd3c45728347.min.css',
+		'admin-js' => 'js/admin.fab33fa.bundle.min.js',
+		'admin-css' => 'css/admin.55cd5352bd3c45728347.min.css',
+		'common-js' => 'js/common.fab33fa.bundle.min.js',
 	);
 	return ! empty( $assets[ $asset ] ) ? $assets[ $asset ] : false;
 }
