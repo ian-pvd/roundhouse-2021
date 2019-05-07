f70d1ee6b2f8829365f163e1e7cf91462ec28949
Ian Del Giudice <ian.pvd@gmail.com>
2019-05-07 00:13:23 -0400

    Markup changes for post thumbnails.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -121,10 +121,12 @@ if ( ! function_exists( 'pvd_post_thumbnail' ) ) :
 	/**
 	 * Displays an optional post thumbnail.
 	 *
-	 * Wraps the post thumbnail in an anchor element on index views, or a div
+	 * Wraps the post thumbnail in an anchor element on list views, or a div
 	 * element when on single views.
+	 *
+	 * @param  string $size The image size to use.
 	 */
-	function pvd_post_thumbnail() {
+	function pvd_post_thumbnail( $size = 'post-thumbnail' ) {
 		if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
 			return;
 		}
@@ -133,26 +135,52 @@ if ( ! function_exists( 'pvd_post_thumbnail' ) ) :
 			?>
 
 			<figure class="post-thumbnail">
-				<?php the_post_thumbnail(); ?>
+				<div class="post-thumbnail__frame">
+					<?php the_post_thumbnail(); ?>
+				</div>
 
 				<?php if ( get_the_post_thumbnail_caption() ) : ?>
-				<figcaption><?php echo esc_html( get_the_post_thumbnail_caption() ); ?></figcaption>
+				<figcaption>
+					<?php
+						echo wp_kses(
+							get_the_post_thumbnail_caption(),
+							[
+								'a'      => [
+									'href'   => [],
+									'rel'    => [],
+									'target' => [],
+								],
+								'b'      => [],
+								'strong' => [],
+								'i'      => [],
+								'em'     => [],
+							]
+						);
+					?>
+				</figcaption>
 				<?php endif; ?>
 			</figure><!-- .post-thumbnail -->
 
 		<?php else : ?>
 
-		<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
-			<?php
-			the_post_thumbnail( 'post-thumbnail', array(
-				'alt' => the_title_attribute( array(
-					'echo' => false,
-				) ),
-			) );
-			?>
-		</a>
+			<div class="post-thumbnail">
+				<a class="post-thumbnail__link" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
+					<?php
+					the_post_thumbnail(
+						$size,
+						[
+							'alt' => the_title_attribute(
+								[
+									'echo' => false,
+								]
+							),
+						]
+					);
+					?>
+				</a>
+			</div>
 
-		<?php
+			<?php
 		endif; // End is_singular().
 	}
 endif;
