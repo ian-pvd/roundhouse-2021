2ce61965c6078e8f9ecd2ef4f6234578274d3c9c
Ian Del Giudice <ian.pvd@gmail.com>
2019-05-12 23:58:25 -0400

    WIP new styles before sync with dev-22 fixes

--- footer.php
+++ footer.php
@@ -15,24 +15,34 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-footer__wrapper">
-			<nav id="footer-navigation" class="site-footer__navigation footer-navigation">
-				<?php
+			<?php
+			if ( has_nav_menu( 'utilities' ) ) :
 				wp_nav_menu(
 					[
-						'container_class' => 'footer-navigation__utilities-menu utilities-menu',
-						'menu_class'     => 'utilities-menu__menu',
-						'menu_id'        => 'utilities-menu__menu',
-						'theme_location' => 'utilities-menu',
+						'container_class' => 'footer-navigation__utilities-menu footer-utilities',
+						'menu_class'      => 'footer-utilities__menu',
+						'menu_id'         => 'footer-utilities__menu',
+						'theme_location'  => 'utilities',
 					]
 				);
-				?>
-			</nav>
-			<div class="site-info copyright">
+			endif;
+			?>
+			<div class="site-info site-info__copyright copyright">
+				<span class="copyright__year">&copy; <?php echo esc_html( date( 'Y' ) ); ?></span>
 				<span class="copyright__author">
-					<a href="<?php echo esc_html( get_bloginfo( 'url' ) ); ?>"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
+					<a href="<?php echo esc_html( home_url() ); ?>"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
 				</span>
-				<span class="copyright__year"><?php echo esc_html( date( 'Y' ) ); ?></span>
-			</div><!-- .site-info -->
+				<span class="copyright__notice">
+					<?php esc_html_e( 'Copyright notice.', 'roundhouse' ); ?>
+				</span>
+			</div><!-- .site-info.copyright -->
+			<div class="site-info site-info__terms-privacy terms-privacy">
+				<?php /* Add terms link with pvd_get_page. */ ?>
+				<?php if ( get_privacy_policy_url() ) : ?>
+				<span class="site-info__privacy"><a href="<?php echo esc_url( get_privacy_policy_url() ); ?>"><?php esc_html_e( 'Privacy Policy', 'gothamish' ); ?></a></span>
+				<?php endif; ?>
+			</div><!-- .site-info.terms-privacy -->
+
 		</div><!-- .site-footer__wrapper -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
